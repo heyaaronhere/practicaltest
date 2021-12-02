2803d3752033e467a1e5951b61c9e12246e9a49f
HeyAaronHere <aaron.chua@hotmail.com>
2021-12-02 15:49:50 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,50 +1,41 @@
-<?php 
-	session_start();
-	
-	if(isset($_POST['submit']))
-	{
-		if((isset($_POST['email']) && $_POST['email'] !='') && (isset($_POST['password']) && $_POST['password'] !=''))
-		{
-				if (strpos($_POST['Search'],'<script>') !== false) { // attack
-		
-				  $_POST['Search'] ="";
-				}
-				else{
-					$_SESSION['Search'] =  $_POST['Search'];
-					header('location:dashboard.php');
-				 exit;
-				}
-	}
-?>
+<?php
+session_start();
+if(isset($_POST['submit']))
+{
+//	if((isset($_POST['Search'] !='')){
+		if (strpos($_POST['Search'],'<script>') !== false) { // attack
 
+		//	echo '<script>alert("attacked by Prans")</script>';
+  		$_POST['Search'] ="";
+		}
+		else{
+			$_SESSION['Search'] =  $_POST['Search'];
+			header('location:dashboard.php');
+		 exit;
+		}
+}
+
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>3x03 Practical </title>
-<link rel="stylesheet" href="style.css">
+<title>Search Page</title>
+
 </head>
 
 <body>
-	
+
 	<div class="container">
-		<h1>Practical Test</h1>
-		<?php 
-			if(isset($errorMsg))
-			{
-				echo "<div class='error-msg'>";
-				echo $errorMsg;
-				echo "</div>";
-				unset($errorMsg);
-			}
-		?>
-		<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-			<table>
-				<tr>
-					<td><input type="text" name="k" value="<?php echo isset($_GET['k']) ? $_GET['k'] : ''; ?>" placeholder="Enter your search keywords" /></td>
-					<td><input type="submit" name="" value="search" /></td>
-				</tr>
-			</table>
+		<form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+				<label>Search</label>
+				<input type="text" name="Search">
+			</div>
+			<div class="field-container">
+				<button type="submit" name="submit">Search</button>
+			</div>
+
 		</form>
 	</div>
 </body>
+
 </html>
